4e24e03b27594fdcf9538316b0692042759487c0
miqdad06 <73184117+miqdad06@users.noreply.github.com>
2020-11-04 18:50:41 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,23 @@
+class chain{
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 1,
+            length: 10
+         }
+     
+         this.chain =Constraint.create(options);
+         World.add(world,chain); 
+    }
+  
+    display() {
+      var pointA=this.chain.bodyA.position;
+      var pointB=this.chain.bodyB.position;
+      strokeWeight(3);
+      strokeWeight(3);
+      line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+  }
+ 
+    
